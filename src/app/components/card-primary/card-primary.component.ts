784a9265bc3e675678f6586cabb4902c52f83e33
JLpensador <joaoluizborgesmorais@gmail.com>
2024-09-24 21:44:39 -0300

    Feat: Adiciona component Card-primary ao projeto

---
+++ src/app/components/card-primary/card-primary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-primary',
+  templateUrl: './card-primary.component.html',
+  styleUrl: './card-primary.component.scss'
+})
+export class CardPrimaryComponent {
+
+}
