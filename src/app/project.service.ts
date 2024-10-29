1742256b92e2dcf5956429da87dfbab9f3f25c0f
João Luiz B. Morais <127153172+JLpensador@users.noreply.github.com>
2024-10-29 13:27:09 -0300

    Feat: Adiciona Cards de Projetos para o portfolio

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -8,107 +8,146 @@ export class ProjectService {
   getProjects() {
     return [
       {
-        name: 'Calculadora de Pirâmide Hexagonal',
+        name: 'Doll',
+        image: 'doll.gif',
+        description:
+          'Um Site de doação de objetos feito unicamente para o evento Mandroid feito pela minha  Escola Estadual Profissionalizante Mário Alencar.',
+        link: 'https://jlpensador.github.io/DOLL/',
+      },
+
+      {
+        name: 'Efeito Matrix',
+        image: 'matrix.gif',
+        description:
+          'Um efeito inspirado na série de filmes Matrix. Criado usando Python, simula a chuva de caracteres verdes caindo em colunas, característica marcante do estilo visual da série.',
+        link: 'https://github.com/JLpensador/Efeito_matrix',
+      },
+
+      {
+        name: 'Gerenciador de Produtos',
+        image: 'gerenciadorProdutos.gif',
+        description:
+          'Um gerenciador para administração e gerenciamento de produtos diversos. Desenvolvido para simplificar o controle de estoque e organização de categorias',
+        link: 'https://github.com/JLpensador/gerenciadorProdutos',
+      },
+
+      {
+        name: 'Hot Wheels AcceleRacers',
+        image: 'hotWheels.gif',
+        description:
+          'Um carrossel de imagens dinâmico da série de filmes Hot Wheels Acceleracers. com transições suaves e controles intuitivos para aprimorar a experiência de visualização.',
+        link: 'https://jlpensador.github.io/Hot_Wheels_AcceleRacers/',
+      },
+      {
+        name: 'Informante News',
+        image: 'informanteNews.png',
+        description:
+          'informanteNews é um projeto desenvolvido para fornecer uma interface moderna e intuitiva para acessar as últimas notícias de várias fontes.',
+        link: 'https://informantenews.netlify.app',
+      },
+
+      {
+        name: 'Jogo da Cobrinha',
+        image: 'cobrinha.gif',
+        description:
+          'Um jogo interativo inspirado no clássico Jogo da Cobrinha, desenvolvido com HTML, CSS e JavaScript. Estilo retrô com uma implementação moderna para navegadores.',
+        link: 'https://jlpensador.github.io/Jogo_da_Cobrinha/',
+      },
+
+      {
+        name: 'Jogo da Memória',
+        image: 'memoria.gif',
+        description:
+          'Um jogo da memória com tema de carros, desenvolvido com HTML, CSS e JavaScript. Neste jogo, o objetivo é encontrar os pares de cartas correspondentes.',
+        link: 'https://jlpensador.github.io/Jogo_da_Memoria/',
+      },
+
+      {
+        name: 'MA Soluções',
+        image: 'MA.gif',
+        description:
+          'Oferecer a comunidade escolar oportunidades para uma qualificação profissional gratuita de excelência em troca de experiências nas áreas da tecnológica',
+        link: 'jlpensador.github.io/MA_Solucoes/',
+      },
+
+      {
+        name: 'Midnight 3',
+        image: 'midnight.gif',
+        description:
+          'Midnight Club 3: DUB Edition é um jogo eletrônico de corrida desenvolvido pela Rockstar San Diego e publicado pela Rockstar Games.',
+        link: 'https://jlpensador.github.io/Midnight_Club_3/',
+      },
+
+      {
+        name: 'Pesquisador de Imagens',
+        image: 'pesquisarImagens.gif',
+        description:
+          'Um projeto de pesquisa de imagens desenvolvido com HTML, CSS e JavaScript. Esse projeto permite buscar por imagens utilizando uma interface amigável e intuitiva.',
+        link: 'https://jlpensador.github.io/Pesquisador_de_imagens/',
+      },
+
+      {
+        name: 'Nirvana',
+        image: 'nirvana.gif',
+        description:
+          'Nirvana foi uma banda de rock formada pelo vocalista e guitarrista Kurt Cobain e pelo baixista Krist Novoselic em Aberdeen no ano de 1987. ',
+        link: 'https://jlpensador.github.io/Nirvana/',
+      },
+
+      {
+        name: 'Pirâmide Hexagonal',
+        image: 'calculadora_piramide.gif',
         description:
           'Um projeto de matemática sobre uma calculadora de pirâmide hexagonal, onde você calcula a área da base, área lateral, área total e o seu volume | Feito com HTML, CSS, Javascript e Python (terminal).',
         link: 'https://jlpensador.github.io/Calculadora_Piramide_Hexagonal/',
       },
 
       {
-        name: 'Doll',
+        name: 'Pokedex',
+        image: 'pokedex.gif',
         description:
-          'Um Site de doação de objetos feito unicamente para o evento Mandroid feito pela minha  Escola Estadual Profissionalizante Mário Alencar. Esse repositório tem como objetivo promover o desenvolvimento de um projeto de doações online feito com Html, Css, JavaScript e Json.',
-        link: 'https://jlpensador.github.io/DOLL/',
-      },
-
-      {
-        name: 'Efeito Matrix',
-        description:
-          'Um efeito estilo a série de filmes matrix. Feito com python.',
-        link: 'https://github.com/JLpensador/Efeito_matrix',
-      },
-
-      {
-        name: 'Gerenciador de Produtos',
-        description:
-          'Um Gerenciador para a administração e gerenciamentos de produtos diversos.',
-        link: 'https://github.com/JLpensador/gerenciadorProdutos',
-      },
-
-      {
-        name: 'Hot Wheels AcceleRacers',
-        description:
-          'Um carrossel de imagens da série de filmes hot wheels Acceleracers.',
-        link: 'https://jlpensador.github.io/Hot_Wheels_AcceleRacers/',
-      },
-      {
-        name: 'Informante News',
-        description:
-          'informanteNews é um projeto desenvolvido para fornecer uma interface moderna e intuitiva para acessar as últimas notícias de várias fontes. O aplicativo permite a visualização de headlines e artigos de notícias de diversos jornais e mídias.',
-        link: 'https://informantenews.netlify.app',
-      },
-
-      {
-        name: 'Jogo da Cobrinha',
-        description:
-          'Um Jogo inspirado no clássico jogo da Cobrinha feito com Html. Css e JavaScript.',
-        link: 'https://jlpensador.github.io/Jogo_da_Cobrinha/',
+          'Uma Pokédex interativa inspirada na série de jogos e filmes Pokémon, desenvolvida com HTML, CSS e JavaScript. Com um design que remete à icônica Pokédex da série',
+        link: 'https://jlpensador.github.io/Pokedex/',
       },
 
       {
         name: 'Pomodoro Timer',
+        image: 'pomodoro.gif',
         description:
-          'Este é um projeto de Pomodoro Timer desenvolvido com Angular. O Pomodoro Timer é uma técnica de gerenciamento de tempo que utiliza blocos de trabalho seguidos de pausas curtas, ajudando a melhorar a produtividade e o foco.',
+          'Este é um projeto de Pomodoro Timer desenvolvido com Angular. O Pomodoro Timer é uma técnica de gerenciamento de tempo que utiliza blocos de trabalho seguidos de pausas curtas.',
         link: 'pomodoro-timer-one-swart.vercel.app',
       },
 
       {
-        name: 'MA Soluções',
+        name: 'Previsão do Tempo',
+        image: 'tempo.gif',
         description:
-          'Oferecer a comunidade escolar oportunidades para uma qualificação profissional gratuita de excelência incentivada pela aprendizagem e pelo desenvolvimento de ideias com troca de experiências nas mais diversas áreas das tecnológicas presentes na sociedade por meio da solidariedade com o compromisso e responsabilidade social.',
-        link: 'jlpensador.github.io/MA_Solucoes/',
+          'Um projeto de previsão do tempo desenvolvido com HTML, CSS e JavaScript, que utiliza uma API para fornecer informações meteorológicas em tempo real.',
+        link: 'https://jlpensador.github.io/previsaoTempo/',
       },
 
       {
         name: 'Quarto 3D',
+        image: 'quarto.gif',
         description:
           'Um quarto 3D onde você pode mexer a sua tela para poder movimenta-lo e ver de outros ângulos. Feito com Html, Css e Javascript.',
         link: 'jlpensador.github.io/Quarto_3D/',
       },
 
       {
-        name: 'Pesquisador de Imagens',
-        description:
-          'Um projeto no qual você pode pesquisar por imagens. Feito com Html, Css, e Javascript.',
-        link: 'https://jlpensador.github.io/Pesquisador_de_imagens/',
-      },
-
-      {
-        name: 'Jogo da Memória',
-        description:
-          'Um jogo da Memória com tema de carros, incluindo carros das marcas: Nissan, Koenigsegg, Ferrari, Lamborghini, Toyota, Chevrolet, Mazda, Subaru, McLaren, Ford, Audi, Dmc, Honda, Mitsubishi e McLaren.',
-        link: 'https://jlpensador.github.io/Jogo_da_Memoria/',
-      },
-
-      {
-        name: 'Previsão do Tempo',
-        description:
-          'Um Projeto de Previsão de Tempo utilizado com API feito com Html, Css e JavaScript.',
-        link: 'https://jlpensador.github.io/previsaoTempo/',
-      },
-
-      {
         name: 'Organizador de Arquivos',
+        image: 'organizadorArquivos.png',
         description:
-          'Um programa de automação desenvolvido em Python, destinado à organização de arquivos de naturezas diversas, apresenta uma solução eficiente para a gestão e ordenação de dados em sistemas computacionais.',
+          'Um programa de automação desenvolvido em Python, criado para organizar arquivos de diferentes naturezas, oferecendo uma solução eficiente para a gestão e ordenação de dados em sistemas computacionais. Esse programa identifica automaticamente o tipo de arquivo (como documentos, imagens, vídeos, etc.) e os organiza em pastas específicas.',
         link: 'https://github.com/JLpensador/Organizador_de_Arquivos',
       },
 
       {
-        name: 'Pokedex',
+        name: 'Tocador de Músicas',
+        image: 'musica.gif',
         description:
-          'Uma pokedex da série de jogos e filmes Pokemon. Feito com Html, Css, e JavaScript',
-        link: 'https://jlpensador.github.io/Pokedex/',
+          'Este projeto permite ao usuário reproduzir, pausar e navegar entre faixas de áudio, oferecendo uma interface intuitiva e atraente.',
+        link: 'https://jlpensador.github.io/Tocador_de_musicas/',
       },
     ];
   }
